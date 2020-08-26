6b1f3f3c011baf574118d11fcc53fff96b97b7a8
enorvang <espen.norvang@gmail.com>
2020-08-26 22:22:42 +0200

    auto-formatted code

--- script.js
+++ script.js
@@ -5,72 +5,73 @@ let text = document.getElementById("text");
 let playerText = document.getElementById("player");
 let computerText = document.getElementById("computer");
 
-function computerPlay(){
-    const alternativer = ['rock', 'paper', 'scissors'];
-    let valg = Math.floor(Math.random() * alternativer.length);
-    return alternativer[valg];
+function computerPlay() {
+  const alternativer = ["rock", "paper", "scissors"];
+  let valg = Math.floor(Math.random() * alternativer.length);
+  return alternativer[valg];
 }
 
-function playGame(playerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerPlay().toLowerCase();
+function playGame(playerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  const computerSelection = computerPlay().toLowerCase();
 
-    playerText.innerHTML = "You: " + playerSelection;
-    computerText.innerHTML = "Computer: " + computerSelection;
+  playerText.textContent = "You chose: " + playerSelection;
+  computerText.textContent = "Computer chose: " + computerSelection;
 
-    switch(playerSelection){
-        case 'rock':
-            switch(computerSelection){
-                case 'rock':
-                    text.innerHTML = 'Draw';
-                    break;
-                case 'paper':
-                    text.innerHtml =  'You lose! Paper beats Rock!';
-                    break;
-                case 'scissors':
-                    text.innerHTML = 'You win! Rock beats Scissors!';
-                    break;
-                }
-            break;
-        case 'paper':
-            switch(computerSelection){
-                case 'rock':
-                    text.innerHTML = 'You win! Paper beats Rock';
-                    break;
-                case 'paper':
-                    text.innerHTML = 'Draw!';
-                    break;
-                case 'scissors':
-                    text.innerHTML = 'You lose! Scissors beats Paper!';
-                    break;
-            }
-            break;
-        case 'scissors':
-            switch(computerSelection){
-                case 'rock':
-                    text.innerHTML = 'You lose! Rock beats Scissors';
-                    break;
-                case 'paper':
-                    text.innerHTML = 'You win! Scissors beats Paper!';
-                    break;
-                case 'scissors':
-                    text.innerHTML = 'Draw!';
-                    break;
-            }
-            break;
-        default:
-                text.innerHTML = 'ERROR! Player selection illegal. Only \'rock\', \'paper\' or \'scissors\' is allowed.';
-    }
+  switch (playerSelection) {
+    case "rock":
+      switch (computerSelection) {
+        case "rock":
+          text.textContent = "Draw";
+          break;
+        case "paper":
+          text.textContent = "You lose! Paper beats Rock!";
+          break;
+        case "scissors":
+          text.textContent = "You win! Rock beats Scissors!";
+          break;
+      }
+      break;
+    case "paper":
+      switch (computerSelection) {
+        case "rock":
+          text.textContent = "You win! Paper beats Rock";
+          break;
+        case "paper":
+          text.textContent = "Draw!";
+          break;
+        case "scissors":
+          text.textContent = "You lose! Scissors beats Paper!";
+          break;
+      }
+      break;
+    case "scissors":
+      switch (computerSelection) {
+        case "rock":
+          text.textContent = "You lose! Rock beats Scissors";
+          break;
+        case "paper":
+          text.textContent = "You win! Scissors beats Paper!";
+          break;
+        case "scissors":
+          text.textContent = "Draw!";
+          break;
+      }
+      break;
+    default:
+      text.textContent =
+        "ERROR! Player selection illegal. Only 'rock', 'paper' or 'scissors' is allowed.";
+  }
 }
 
-rock.addEventListener('click', function(){
-    playGame("rock");
-})
+rock.addEventListener("click", function () {
+  playGame("rock");
+});
 
-paper.addEventListener('click', function() {
-    playGame("paper");
-})
+paper.addEventListener("click", function () {
+  playGame("paper");
+});
 
-scissors.addEventListener('click', function(){
-    playGame("scissors");
-})
+scissors.addEventListener("click", function () {
+  playGame("scissors");
+});
