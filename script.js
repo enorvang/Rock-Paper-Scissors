c26729c8ed7407aa6db8a7e2e225def80af1be00
enorvang <espen.norvang@gmail.com>
2020-08-26 23:01:15 +0200

    added some new functionality

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let rock = document.getElementById("rock");
-let paper = document.getElementById("paper");
-let scissors = document.getElementById("scissors");
-let text = document.getElementById("text");
-let playerText = document.getElementById("player");
-let computerText = document.getElementById("computer");
+"use strict";
+
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const text = document.getElementById("text");
+const playerText = document.getElementById("player");
+const computerText = document.getElementById("computer");
+const playerScore = document.getElementById("playerScore");
+const computerScore = document.getElementById("computerScore");
+const scoresContainer = document.querySelector("#scores");
+
+var numPlayerWins = 0;
+var numComputerWins = 0;
 
 function computerPlay() {
   const alternativer = ["rock", "paper", "scissors"];
@@ -25,9 +33,11 @@ function playGame(playerSelection) {
           text.textContent = "Draw";
           break;
         case "paper":
+          numComputerWins++;
           text.textContent = "You lose! Paper beats Rock!";
           break;
         case "scissors":
+          numPlayerWins++;
           text.textContent = "You win! Rock beats Scissors!";
           break;
       }
@@ -35,12 +45,14 @@ function playGame(playerSelection) {
     case "paper":
       switch (computerSelection) {
         case "rock":
+          numPlayerWins++;
           text.textContent = "You win! Paper beats Rock";
           break;
         case "paper":
           text.textContent = "Draw!";
           break;
         case "scissors":
+          numComputerWins++;
           text.textContent = "You lose! Scissors beats Paper!";
           break;
       }
@@ -48,9 +60,11 @@ function playGame(playerSelection) {
     case "scissors":
       switch (computerSelection) {
         case "rock":
+          numComputerWins++;
           text.textContent = "You lose! Rock beats Scissors";
           break;
         case "paper":
+          numPlayerWins++;
           text.textContent = "You win! Scissors beats Paper!";
           break;
         case "scissors":
@@ -62,6 +76,38 @@ function playGame(playerSelection) {
       text.textContent =
         "ERROR! Player selection illegal. Only 'rock', 'paper' or 'scissors' is allowed.";
   }
+
+  computerScore.textContent = numComputerWins;
+  playerScore.textContent = numPlayerWins;
+
+  if (numPlayerWins === 5) {
+    alert("You reached 5 points, you win!");
+    showResetButton();
+  }
+  if (numComputerWins === 5) {
+    alert("Computer reached 5 points, you lose!");
+    showResetButton();
+  }
+}
+
+function showResetButton() {
+  const newButton = document.createElement("button");
+  newButton.setAttribute("id", "newButton");
+  newButton.innerHTML = "Reset Game";
+  newButton.addEventListener("click", resetGame);
+  scoresContainer.appendChild(newButton);
+}
+
+function resetGame() {
+  text.textContent = "";
+  numComputerWins = 0;
+  numPlayerWins = 0;
+  playerText.textContent = "";
+  computerText.textContent = "";
+  computerScore.textContent = "";
+  playerScore.textContent = "";
+  const newButton = document.querySelector("#newButton");
+  scoresContainer.removeChild(newButton);
 }
 
 rock.addEventListener("click", function () {
